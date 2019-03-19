47f7605c5e24ace9aa70eedf46ceb34cb26544c7
kavi0310 <46838903+kavi0310@users.noreply.github.com>
2019-03-19 21:52:57 +0530

    Create p84.c

---
+++ p84.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    unsigned int a[1000],i,n,add;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    if(n!=1)
+    {
+    for(i=0;i<n;i++)
+    {
+    add=a[i]|a[i+1];
+    break;
+    }
+    printf("%d",add);
+    }
+    else
+    printf("%d",a[0]);
+}
